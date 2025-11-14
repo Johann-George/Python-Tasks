c4456ba6cbb21e820e47c2fc9ed8478287033db4
Johann George <johanngeorge2003@gmail.com>
2025-11-14 10:47:44 +0530

    Added loggers

--- Day_11_DB/logger.py
+++ Day_11_DB/logger.py
@@ -13,3 +13,7 @@ logging.info("This is an info message.")
 logging.warning("This is a warning message.")
 logging.error("This is an error message.")
 logging.critical("This is a critical message.")
+
+# logger = logging.getLogger('johann')
+# logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+
--- Day_15_FastAPI/main.py
+++ Day_15_FastAPI/main.py
@@ -4,27 +4,30 @@ import traceback
 import sys
 import os
 import json
-from typing import List
+import logging
 from model.models import Ocr_Status
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from Day_7_OCR.main import OCR
 from crud.ocr import OCRService
-from model.models import ocr_data
 
 app = FastAPI()
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(message)s')
 
-@app.get("/")
+@app.get("/api/v1/ocr/")
 async def greet():
+    logging.info("Demo greeting")
     return {"message": "Hello!"}
 
 @app.post("/api/v1/ocr/store")
 async def extract_df(file: UploadFile = File(...)):
     try:
+        logging.info("Post API invoked")
         upload_dir = "uploads"
         os.makedirs(upload_dir, exist_ok=True)
         file_path = os.path.join(upload_dir, file.filename)
-        print(file_path)
         with open(file_path, "wb") as buffer:
             while content := await file.read(1024 * 1024):
                 buffer.write(content)
@@ -36,17 +39,17 @@ async def extract_df(file: UploadFile = File(...)):
             tesseract_config='--psm 6 --oem 1',
             min_conf=0,
             show=False,
-            # header_keywords=['NAME', 'CHILDREN', 'AGE', 'ADDRESS', 'GUARDIAN', 'PARENT'],
             table_start_row=17,
-            refine_data=True  # Enable data refinement/cleaning
+            refine_data=True  
         )
+        
         df, extractor = pipeline.run()
-        print("?"*80)
-        print(df.to_json(orient='records'))
         OCRService.create_ocr(file_name=file.filename, ocr_data=df.to_json(orient='records'), ocr_status=Ocr_Status("Success"), created_at=datetime.now())
-        return {"message":"Successfully updated the data to the db!"}
+        logging.info("Successfully stored the value in the db")
+        return {"message":"Successfully stored the data in the db!"}
 
     except Exception as e:
+        OCRService.create_ocr(file_name=file.filename, ocr_data=df.to_json(orient='records'), ocr_status=Ocr_Status("Failed"), created_at=datetime.now())
         print("An unexpected error occured main():",e)
         traceback.print_exc()
 
@@ -56,26 +59,31 @@ async def extract_df(file: UploadFile = File(...)):
 @app.get("/api/v1/ocr/{id}")
 async def get_details(id: int):
     try:
-        return  OCRService.get_ocr(id)
+        logging.info("Get API invoked!")
+        return OCRService.get_ocr(id)
     except Exception as e:
         print("An error occured get_details():", e)
 
-@app.put("/api/v1/ocr/{id}")
+@app.put("/api/v1/ocr/{id}", response_model=dict)
 async def update_details(id: int, request: Request):
     try:
+        logging.info("Put API invoked")
         raw_body = await request.body()
         json_string = raw_body.decode("utf-8")
         data = json.loads(json_string)
-        print("?"*80)
-        print(data)
+        logging.info("Retrieved the data from the request body")
         OCRService.update_ocr(id, data)
+        logging.info("Data updated successfully to the db")
+        return {"message": "Row updated successfully"}
     except Exception as e:
         print("An error occured update_details():",e)
 
-@app.delete("/api/v1/ocr/{id}")
+@app.delete("/api/v1/ocr/{id}", response_model=dict)
 async def delete_details(id: int):
     try:
+        logging.info("Delete API invoked")
         OCRService.delete_ocr(id)
+        logging.info("Data deleted successfully")
         return {"message": "Row successfully deleted"}
     except Exception as e:
         print("An error occured delete_details():", e)
